0c80123dd92824b0205a1299ff9d10e058902098
半雨微凉 <499836921@qq.com>
2020-08-24 15:35:26 +0800

    加入了详细的过程日志

--- src/main/java/com/banywl/file/transfer/udp/Application.java
+++ src/main/java/com/banywl/file/transfer/udp/Application.java
@@ -8,15 +8,16 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class Application {
 
-    private static final LinkedBlockingQueue<File> FILE_QUEUE = new LinkedBlockingQueue<File>(20);
+    private static final LinkedBlockingQueue<File> FILE_QUEUE = new LinkedBlockingQueue<File>();
 
     /**
      * -server 18888 10240
      * -client 255.255.255.255 18888 10240 path/to/file
+     *
      * @param args
      */
     public static void main(String[] args) {
-        String baseDir = System.getProperty("user.dir") ;
+        String baseDir = System.getProperty("user.dir");
         System.out.println(baseDir);
         if (args.length == 0) {
             System.out.println("参数错误!");
@@ -28,40 +29,40 @@ public class Application {
             int port = Integer.parseInt(args[1]);
             int size = args.length == 2 ? 1024 : Integer.parseInt(args[2]);
             try {
-                UDPFileServer server = new UDPFileServer(port,size);
-                while (true){
+                UDPFileServer server = new UDPFileServer(port, size);
+                while (true) {
                     System.out.println("等待接收");
                     server.receiveFile();
-                    File file = new File(baseDir + File.separator+ "receive" + File.separator+server.getFileName());
-                    if (!file.getParentFile().exists()){
-                        file.getParentFile().mkdir();
+                    File file = new File(baseDir + File.separator + "receive" + File.separator + server.getFileName());
+                    if (!file.getParentFile().exists()) {
+                        file.getParentFile().mkdirs();
                     }
                     FileOutputStream fo = new FileOutputStream(file);
                     fo.write(server.getFileData());
                     fo.close();
-                    System.out.println("文件已写入磁盘:"+file.getAbsolutePath());
+                    System.out.println("文件已写入磁盘:" + file.getAbsolutePath());
                 }
             } catch (SocketException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }else if("-client".equals(args[0])){
+        } else if ("-client".equals(args[0])) {
             System.out.println("启动客户端");
             String ip = args[1];
             int port = Integer.parseInt(args[2]);
             int size = Integer.parseInt(args[3]);
             String pathname = args[4];
-            System.out.println("扫描目录文件:"+pathname);
+            System.out.println("扫描目录文件:" + pathname);
             listFile(pathname);
             System.out.println("开始发送文件");
             try {
-                UDPFileClient client = new  UDPFileClient(ip,port,size);
+                UDPFileClient client = new UDPFileClient(ip, port, size);
                 File file = null;
-                while ((file = FILE_QUEUE.poll()) != null){
-                    String filename = file.getAbsolutePath().replace(pathname,"");
-                    client.sendFile(pathname,filename);
-                    System.out.println("发送完成："+file.getAbsolutePath());
+                while ((file = FILE_QUEUE.poll()) != null) {
+                    String filename = file.getAbsolutePath().replace(pathname, "");
+                    client.sendFile(pathname, filename);
+                    System.out.println("发送完成：" + file.getAbsolutePath());
                 }
                 client.close();
                 System.out.println("文件发送结束，客户端退出");
@@ -69,7 +70,7 @@ public class Application {
                 e.printStackTrace();
             }
 
-        }else{
+        } else {
             System.out.println("参数错误!");
             System.exit(0);
         }
@@ -78,14 +79,16 @@ public class Application {
 
     /**
      * 递归获取目录下的文件名
+     *
      * @param pathname
      */
-    public static void listFile(String pathname){
+    public static void listFile(String pathname) {
         File dir = new File(pathname);
         for (File file : dir.listFiles()) {
-            if (file.isDirectory()){
+            if (file.isDirectory()) {
                 listFile(file.getAbsolutePath());
-            }else{
+            } else {
+                System.out.println("装载文件:" + file.getName());
                 try {
                     FILE_QUEUE.put(file);
                 } catch (InterruptedException e) {
@@ -95,7 +98,6 @@ public class Application {
             }
         }
     }
-
 
 
 }
